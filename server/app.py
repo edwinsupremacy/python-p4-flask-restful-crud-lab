69361e42e7406ff1fb11b22ad09032878178c6d5
edwinsupremacy <edwinmwangi837@gmail.com>
2024-07-05 16:02:53 +0300

    finished lab

--- server/app.py
+++ server/app.py
@@ -1,54 +1,60 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
-from models import db, Plant
+from models import db, Plant  
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json_encoder = None
 
 migrate = Migrate(app, db)
+
 db.init_app(app)
 
-api = Api(app)
+
+@app.route('/plants')
+def get_plants():
+    plants = [plant.to_dict() for plant in Plant.query.all()]
+    return jsonify(plants)
 
 
-class Plants(Resource):
+@app.route('/plants', methods=['POST'])
+def create_plant():
+    data = request.get_json()
+    new_plant = Plant(name=data['name'],image=data['image'],price=data['price'],is_in_stock=data.get('is_in_stock', True))
+    db.session.add(new_plant)
+    db.session.commit()
+    return jsonify(new_plant.to_dict())
 
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
-
-    def post(self):
-        data = request.get_json()
-
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
-
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+@app.route('/plants/<int:id>')
+def plant(id):
+    plant = Plant.query.get(id)
+    if not plant:
+        return jsonify({'error': 'Plant not found'})
+    else:
+        return jsonify(plant.to_dict())
+        
+@app.route('/plants/<int:id>', methods=['PATCH'])
+def patch_plant(id):
+    plant = Plant.query.get(id)
+    if not plant:
+        return jsonify({'error': 'Plant not found'})
+    
+    data = request.get_json()
+    for key, value in data.items():
+        setattr(plant, key, value)
+    db.session.commit()
+    return jsonify(plant.to_dict()), 200
 
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+@app.route('/plants/<int:id>', methods=['DELETE'])
+def delete_plant(id):
+    plant = Plant.query.get(id)
+    if not plant:
+        return jsonify({'error': 'Plant not found'})
+    db.session.delete(plant)
+    db.session.commit()
+    return ''
 
 
 if __name__ == '__main__':
